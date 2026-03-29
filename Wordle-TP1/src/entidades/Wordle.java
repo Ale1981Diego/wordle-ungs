6de5f65c5da8abb243a3357d702f4d31950a0caf
Ale1981Diego <mancilla.alejandro@hotmail.com>
2026-03-28 23:14:34 -0300

    Se agregan archivos.txt con las palabras que se utilizarán para el juego.

---
+++ Wordle-TP1/src/entidades/Wordle.java
@@ -0,0 +1,21 @@
+package entidades;
+
+public class Wordle 
+{
+	private Usuario usuario;
+	private Palabra palabra;
+	
+	public Wordle()
+	{
+		this.usuario = null;
+		this.palabra = null;
+	}
+
+	public Wordle(Usuario usuario, Palabra palabra)
+	{
+		this.usuario = usuario;
+		this.palabra = palabra;
+	}
+	
+	
+}
